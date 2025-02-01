0193929df6ccab64f28a60aef1069c2a02f2c7dd
mouhamedbouzayan <bouzimohammed177@gmail.com>
2025-02-01 14:24:27 +0100

    CREATION LA BANNER

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -4,6 +4,10 @@ import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import { HashLink } from 'react-router-hash-link';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 
 export const NavBar = () => {
 
@@ -29,6 +33,7 @@ export const NavBar = () => {
   }
 
   return (
+    <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
@@ -49,11 +54,13 @@ export const NavBar = () => {
                 <a href="https://www.facebook.com/profile.php?id=61566829267014" target="_blank"><img src={navIcon2} alt="" /></a>
                 <a href="https://www.instagram.com/siimo.bz9?igsh=MWhqZDUwcXduZTJpcQ==" target="_blank"><img src={navIcon3} alt="" /></a>
               </div>
-             
+              <HashLink to='#connect'>
+                <button className="vvd"><span>Let’s Connect</span></button>
+              </HashLink>
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    
+    </Router>
   )
 }
